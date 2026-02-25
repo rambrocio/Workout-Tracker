647800ca621e8569ca48a54204789ef8663e9719
rambrocio <ra45@njit.edu>
2026-02-25 12:08:37 -0500

    Updated ProfilePage UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { getUserProfile } from "../services/profileServices";
 import { getWorkoutStats } from "../services/workoutServices";
 import Heading from "../components/heading";
-import { background } from "@chakra-ui/react";
+
 
 const Profile = () => {
     const { session, signOut } = UserAuth();
@@ -78,20 +78,26 @@ const Profile = () => {
         <div>
             <Heading />
             <div className="pageContainer">
-                <h2>Welcome to your Profile, {userData?.name || "Loading..."}</h2>
-                <div>
-                    <h3>MAIN STATS</h3>
-                    <p>Weight: {userData?.weight}</p>
-                    <p>Max Bench Press: {userData?.bench_max || "0"} lbs</p>
-                    <p>Max Squat: {userData?.squat_max || "0"} lbs</p>
-                    <p>Max Deadlift: {userData?.deadlift_max || "0"} lbs</p>
-                </div>
-                <div>
-                    <h3>TOTAL STATS:</h3>
-                    <p>Total Days Worked Out: {daysWorkedOut || 0} days</p>
-                    <p>Total Sets Lifted: {totalSets} sets</p>
-                    <p>Total Reps Listed: {totalReps || 0} reps</p>
-                    <p>Total Weight Lifted: {totalWeight || 0} lbs</p>
+                <h2>Welcome To Your Profile, {userData?.name || "Loading..."}</h2>
+                <div className="contentContainer">
+                    <div className="section">
+                        <h2><u>MAIN STATS:</u></h2>
+                        <div className="stats">
+                            <p className="statsText"><b>Weight:</b> {userData?.weight}</p>
+                            <p className="statsText"><b>Max Bench Press:</b> {userData?.bench_max || "0"} lbs</p>
+                            <p className="statsText"><b>Max Squat:</b> {userData?.squat_max || "0"} lbs</p>
+                            <p><b>Max Deadlift:</b> {userData?.deadlift_max || "0"} lbs</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <h2><u>TOTAL STATS:</u></h2>
+                        <div className="stats">
+                            <p className="statsText"><b>Total Days Worked Out:</b> {daysWorkedOut || 0} days</p>
+                            <p className="statsText"><b>Total Sets Lifted:</b> {totalSets} sets</p>
+                            <p className="statsText"><b>Total Reps Listed:</b> {totalReps || 0} reps</p>
+                            <p><b>Total Weight Lifted:</b> {totalWeight || 0} lbs</p>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <button onClick={handleSignOut}>Sign Out</button>
