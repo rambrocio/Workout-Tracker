fcdb584e7f086cc3ef6e6b3fadddbf0e063001aa
rambrocio <ra45@njit.edu>
2026-02-25 14:39:45 -0500

    Created first custom hook. Not implemented yet

---
+++ src/hooks/loadUser.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { getUserProfile } from "../services/profileServices";
+
+function loadUser(session) {
+    const [userData, setUserData] = useState(null);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const loadData = async () => {
+            if (session?.user?.id) {
+                setLoading(true);
+                try {
+                    const data = await getUserProfile(session.user.id);
+                    setUserData(data);
+                    setError(null);
+                } catch (err) {
+                    console.error("Fetch failed:", err);
+                    setError(error.message);
+                }
+                finally {
+                    setLoading(false);
+                }
+            }
+        };
+        loadData(); 
+    }, [session]);
+
+    return { userData, error, loading };
+}
+
+export default loadUser;
